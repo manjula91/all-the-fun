b84a8eeeb6a472d7ef9e507d0510a14d46096486
vihangpatel <vihang.engg@gmail.com>
2020-10-10 16:52:51 +0530

    Generate static site using snap shot generator

--- public/app-sw.js
+++ public/app-sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.f9536b7382c98526ace6c68940468ba4.js"
+  "/build/precache-manifest.5342d73734fb6e39436ff169f8dc9b9c.js"
 );
 
 workbox.skipWaiting();
