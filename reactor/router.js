95625f20d7e74e3601f72763576e6ef8a535b2df
Sahil Mhapsekar <saheel.mhapsekar@gmail.com>
2021-06-27 20:48:10 +0200

    add env flags for remote page

--- reactor/router.js
+++ reactor/router.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Home from './pages/home';
-import Home2021 from './pages/home2021';
-import Subscribe from './pages/subscribe';
-import Tickets from './pages/tickets';
-import Workshop from './pages/workshop';
-import Footer from './footer';
+import Home from "./pages/home";
+import Home2021 from "./pages/home2021";
+import Subscribe from "./pages/subscribe";
+import Tickets from "./pages/tickets";
+import Workshop from "./pages/workshop";
+import Footer from "./footer";
 
-import Root from './pages/root';
+import Root from "./pages/root";
 
-const AppRouter = () => (
-  <>
-    <Switch>
-      <Route path="/" component={Root} exact />
-      <Route path="/conferences/2019" component={Home} exact />
-      <Route path="/conferences/2021" component={Home2021} exact />
-      <Route path="/subscribe" component={Subscribe} exact />
-      <Route path="/tickets" component={Tickets} exact />
-      <Route path="/workshop" component={Workshop} exact />
-    </Switch>
-    <Footer />
-  </>
-);
+const AppRouter = () => {
+
+  console.log("lol",process.env.REMOTE)
+
+  return (
+    <>
+      <Switch>
+        <Route
+          path="/"
+          component={process.env.REMOTE ? Home2021 : Root}
+          exact
+        />
+        <Route path="/conferences/2019" component={Home} exact />
+        <Route path="/conferences/2021" component={Home2021} exact />
+        <Route path="/subscribe" component={Subscribe} exact />
+        <Route path="/tickets" component={Tickets} exact />
+        <Route path="/workshop" component={Workshop} exact />
+      </Switch>
+      <Footer />
+    </>
+  )
+}
 
 export default AppRouter;
