8de613a7fc9e18cd2bcda35590a261b45c1cfbd7
Sahil Mhapsekar <saheel.mhapsekar@gmail.com>
2021-06-27 21:38:05 +0200

    fix master

--- reactor/router.js
+++ reactor/router.js
@@ -13,7 +13,7 @@ import Root from './pages/root';
 const AppRouter = () => (
   <>
     <Switch>
-      <Route path="/" component={process.env.REMOTE === 'Home2021' ? Home2021 : Root} exact />
+      <Route path="/" component={Home2021} exact />
       <Route path="/conferences/2019" component={Home} exact />
       <Route path="/conferences/2021" component={Home2021} exact />
       <Route path="/subscribe" component={Subscribe} exact />
