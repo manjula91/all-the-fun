ce70d77307ef91ddbae920441ba56f3f3fb0b1ec
vihangpatel <vihang.engg@gmail.com>
2021-06-30 16:05:16 +0530

    Header

---
+++ public/build/precache-manifest.3eda231bf0856e243d31d317bbf705f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba2af8b7fac66678819",
+    "url": "/build/vendor.53cec26ee98ac4d827a2.js"
+  },
+  {
+    "revision": "a1a151bd6ea5decf9a48",
+    "url": "/build/style.887a28721d200c47054a.css"
+  },
+  {
+    "revision": "a1a151bd6ea5decf9a48",
+    "url": "/build/style.349e1dd7788fffdd07e1.js"
+  },
+  {
+    "revision": "5331edbc6dcf2d78afa517e28cdb1ca5",
+    "url": "/build/stats.json"
+  },
+  {
+    "revision": "9393bff8a6e651621f9f",
+    "url": "/build/client.bd03a80516c96a9218c9.js"
+  }
+];
